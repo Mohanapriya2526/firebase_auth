23276a957e82b5f8f4087a77c6496936cfca76bb
Mohanapriya2526 <priyadhanush37@gmail.com>
2024-02-06 14:31:50 +0530

    Modified main file

--- main.tf
+++ main.tf
@@ -3,22 +3,44 @@
 #   project = var.project
    
 # }
-resource "google_identity_platform_config" "auth" {
-   provider = google-beta
-   project  = var.project
-  # project = module.firebase_signin.auth_project
-}
+# resource "google_identity_platform_config" "auth" {
+#    provider = google-beta
+#    project  = var.project
+#   # project = module.firebase_signin.auth_project
+# }
 
-output "auth" {
-  description = "The ID of the created Identity Platform configuration."
-  value       = google_identity_platform_config.auth
-}
+# output "auth" {
+#   description = "The ID of the created Identity Platform configuration."
+#   value       = google_identity_platform_config.auth
+# }
 
 # import {
 #   id = "projects/{{edblack}}/config"
 #   to = google_identity_platform_config.auth
 # }
+resource "google_identity_platform_config" "auth" {
+  # provider = google_beta
+  project = var.project
+    sign_in {
+        allow_duplicate_emails = true
 
+        # anonymous {
+        #     enabled = true
+        # }
+
+        email {
+            enabled = true
+            password_required = false
+        }
+
+        phone_number {
+            enabled = true
+            test_phone_numbers = {
+                "+11231231234" = "000000"
+            }
+        }
+    }
+}
 
 module "project_service" {
   source  = "./modules/google_project_service"
--- modules/google_identity_platform_config/output.tf
+++ modules/google_identity_platform_config/output.tf
@@ -1,7 +1,7 @@
-output "auth" {
-  description = "The ID of the created Identity Platform configuration."
-  value       = google_identity_platform_config.auth
-}
+# output "auth" {
+#   description = "The ID of the created Identity Platform configuration."
+#   value       = google_identity_platform_config.auth
+# }
 
 # output "auth_project" {
 #   description = "The project associated with the Identity Platform configuration."
