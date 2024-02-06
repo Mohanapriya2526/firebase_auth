0583409ce7a73970dfa5125c04c3703f03cb8634
Mohanapriya2526 <priyadhanush37@gmail.com>
2024-02-06 14:23:22 +0530

    Modified main file

--- main.tf
+++ main.tf
@@ -3,21 +3,29 @@
 #   project = var.project
    
 # }
-resource "google_identity_platform_config" "auth" {
-   provider = google-beta
-   project  = var.project
-  # project = module.firebase_signin.auth_project
-}
+# resource "google_identity_platform_config" "auth" {
+#    provider = google-beta
+#    project  = var.project
+#   # project = module.firebase_signin.auth_project
+# }
 
-output "auth" {
-  description = "The ID of the created Identity Platform configuration."
-  value       = google_identity_platform_config.auth.name
-}
+# output "auth" {
+#   description = "The ID of the created Identity Platform configuration."
+#   value       = google_identity_platform_config.auth.name
+# }
 
 # import {
 #   id = "projects/{{edblack}}/config"
 #   to = google_identity_platform_config.auth
 # }
+module "identity_config" {
+  source = "./modules/google_identity_platform_config"
+  project = var.project
+}
+
+output "auth" {
+  value = module.identity_config.auth
+}
 module "project_service" {
   source  = "./modules/google_project_service"
   project = var.project
