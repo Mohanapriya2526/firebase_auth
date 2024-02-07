b2ff56e6ffd0c452008f8bdbe51b83b237e23557
Mohanapriya2526 <priyadhanush37@gmail.com>
2024-02-07 15:25:33 +0530

    Modified main file

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ module "google_signin" {
    
   project  = var.project
 #    project = module.firebase_signin.auth_project
+
    depends_on = [ module.project_service ]
 
 }
@@ -20,11 +21,11 @@ module "google_signin" {
 #   description = "The project associated with the Identity Platform configuration."
 #   value       = var.project
 # }
+
 import {
   id = "projects/{{edblack}}/config"
   to = google_identity_platform_config.auth
 }
-
 
 module "project_service" {
   source  = "./modules/google_project_service"
