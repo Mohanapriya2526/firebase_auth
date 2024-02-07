1aeb13ad7b5ec9f4d76878b6dec0194af9012877
Mohanapriya2526 <priyadhanush37@gmail.com>
2024-02-07 16:03:05 +0530

    Modified main file

--- main.tf
+++ main.tf
@@ -5,12 +5,33 @@ module "google_signin" {
 }
  resource "google_identity_platform_config" "auth" {
    provider = google-beta
-   
-  project  = var.project
-#    project = module.firebase_signin.auth_project
-  depends_on = [ module.project_service ]
+    project = var.project
+    sign_in {
+        allow_duplicate_emails = true
 
+        # anonymous {
+        #     enabled = true
+        # }
+
+        email {
+            enabled = true
+            password_required = false
+        }
+
+        phone_number {
+            enabled = true
+            test_phone_numbers = {
+                "+11231231234" = "000000"
+            }
+        }
+    }
 }
+
+#   project  = var.project
+# #    project = module.firebase_signin.auth_project
+#   depends_on = [ module.project_service ]
+
+# }
 
 # output "auth" {
 #   description = "The ID of the created Identity Platform configuration."
